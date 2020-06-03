2d48a52954c78c9711ebd615050c911a653bbaa8
stefanwullems <stefan@exivity.com>
2020-06-03 19:46:15 +0200

    Texture and Asset managers

--- src/Entity.h
+++ src/Entity.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 #include "./EntityManager.h"
 #include "./Component.h"
@@ -15,7 +16,7 @@ class Entity {
     EntityManager& manager;
     bool isActive;
     std::vector<Component*> components;
-  
+    std::map<const std::type_info*, Component*> componentTypeMap;
   public:
     std::string name;
     Entity(EntityManager& manager);
@@ -32,8 +33,16 @@ class Entity {
 
       newComponent->owner = this;
       components.emplace_back(newComponent);
+
+      componentTypeMap[&typeid(*newComponent)] = newComponent; 
+
       newComponent->Initialize();
       return *newComponent;
+    }
+
+    template <typename T>
+    T* GetComponent() {
+      return static_cast<T*>componentTypeMap[&typeid(T)];
     }
 };
 
---
+++ src/TextureManager.h
@@ -0,0 +1,17 @@
+#ifndef TEXTURE_MANAGER_H
+#define TEXTURE_MANAGER_H
+
+#include "./Game.h"
+
+class TextureManager {
+    public:
+      static SDL_Texture* LoadTexture(const char* fileName);
+      static void Draw(
+        SDL_Texture* texture, 
+        SDL_Rect sourceRectangle, 
+        SDL_Rect destinationRectangle,
+        SDL_RendererFlip flip
+      );      
+};
+
+#endif
